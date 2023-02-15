3db4c72fa67b464de9b7223833c1aa520c2f7fba
akshayjedhe1993 <120628350+akshayjedhe1993@users.noreply.github.com>
2023-02-15 23:16:45 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'counter',
+    loadChildren: () =>
+      import('./counter/counter.module').then((m) => m.CounterModule),
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/posts.module').then((m) => m.PostsModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
